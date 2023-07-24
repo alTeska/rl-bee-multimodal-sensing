073b1659a563064341801e8fc1041fb622d64fcd
Aleksandra Teska <aleksandra.teska@gmail.com>
2023-07-24 09:30:39 +0200

    script to locally run multi-run trainig

--- run-multiround-training.sh
+++ run-multiround-training.sh
@@ -2,6 +2,6 @@ source ../../venvdl/bin/activate
 python train_model.py --config_path configs/config-round1.yaml
 python train_model.py --config_path configs/config-round2.yaml
 python train_model.py --config_path configs/config-round3.yaml
-python train_model.py --config_path configs/config-round4.yaml
+# python train_model.py --config_path configs/config-round4.yaml
 python train_model.py --config_path configs/config-round5.yaml
 python train_model.py --config_path configs/config-round6.yaml
